fdbbe945f5d85dbedee177d6721c4178f2d5526f
nikinov <novellenicholas@gmail.com>
2019-10-28 23:41:17 +0100

    Revert "..."
    This reverts commit eb8a6cf66cc4a98fcb7ad80b646ed7d0cbae5db3, reversing
    changes made to 158bb8f0573f1df2adfc440a0dc4f75d58cdd1b9.

--- BALL RACE 0.2/Assets/scripts/InUse/Nikinov/GameManager.cs
+++ BALL RACE 0.2/Assets/scripts/InUse/Nikinov/GameManager.cs
@@ -1,1 +1,83 @@
-﻿using UnityEngine;using UnityEngine.SceneManagement;public class GameManager : MonoBehaviour{    bool GameHasEnded = false;    GameObject CompleteLevel;    GameObject pauseLevel;    GameObject DethPanel;    GameObject GamePanel;    public GameObject Speedo;    private void Start()    {        DethPanel = GameObject.FindGameObjectWithTag("DethPanel");        CompleteLevel = GameObject.FindGameObjectWithTag("CompleteLevel");        pauseLevel = GameObject.FindGameObjectWithTag("PauseMenu");        GamePanel = GameObject.FindGameObjectWithTag("GamePanel");        DethPanel.SetActive(false);        CompleteLevel.SetActive(false);        pauseLevel.SetActive(false);        GamePanel.SetActive(true);        Speedo.SetActive(true);    }    public void completeLevel()    {        CompleteLevel.SetActive(true);        GamePanel.SetActive(false);    }    public void PauseLevel()    {        pauseLevel.SetActive(true);        GamePanel.SetActive(false);        Time.timeScale = 0f;        Speedo.SetActive(false);    }    public void ResumeLevel()    {        pauseLevel.SetActive(false);        GamePanel.SetActive(true);        Time.timeScale = 1f;        Speedo.SetActive(true);    }    public void EndGame()    {        if (GameHasEnded == false)        {            GameHasEnded = true;            Debug.Log("game over");            Deth();            GamePanel.SetActive(false);        }    }    public void Restart()    {        SceneManager.LoadScene(SceneManager.GetActiveScene().name);    }    void Deth()    {        DethPanel.SetActive(true);    }    public void ToMap()    {        SceneManager.LoadScene(sceneBuildIndex: 2);    }    public void ToStore()    {        SceneManager.LoadScene(sceneBuildIndex: 1);        Time.timeScale = 1f;    }    public void GoToMainMenu()    {        SceneManager.LoadScene(sceneBuildIndex: 0);        Time.timeScale = 1f;    }}
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool GameHasEnded = false;
+
+    GameObject CompleteLevel;
+
+    GameObject pauseLevel;
+
+    GameObject DethPanel;
+
+    GameObject GamePanel;
+
+    public GameObject Speedo;
+
+    private void Start()
+    {
+        DethPanel = GameObject.FindGameObjectWithTag("DethPanel");
+        CompleteLevel = GameObject.FindGameObjectWithTag("CompleteLevel");
+        pauseLevel = GameObject.FindGameObjectWithTag("PauseMenu");
+        GamePanel = GameObject.FindGameObjectWithTag("GamePanel");
+        DethPanel.SetActive(false);
+        CompleteLevel.SetActive(false);
+        pauseLevel.SetActive(false);
+        GamePanel.SetActive(true);
+        Speedo.SetActive(true);
+    }
+
+    public void completeLevel()
+    {
+        CompleteLevel.SetActive(true);
+        GamePanel.SetActive(false);
+    }
+    public void PauseLevel()
+    {
+        pauseLevel.SetActive(true);
+        GamePanel.SetActive(false);
+        Time.timeScale = 0f;
+        Speedo.SetActive(false);
+    }
+    public void ResumeLevel()
+    {
+        pauseLevel.SetActive(false);
+        GamePanel.SetActive(true);
+        Time.timeScale = 1f;
+        Speedo.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (GameHasEnded == false)
+        {
+            GameHasEnded = true;
+            Debug.Log("game over");
+            Deth();
+            GamePanel.SetActive(false);
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void Deth()
+    {
+        DethPanel.SetActive(true);
+    }
+    public void ToMap()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 2);
+    }
+    public void ToStore()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+        Time.timeScale = 1f;
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 0);
+        Time.timeScale = 1f;
+    }
+}
