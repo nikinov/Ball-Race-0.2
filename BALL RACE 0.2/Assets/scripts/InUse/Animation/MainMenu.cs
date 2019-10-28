158bb8f0573f1df2adfc440a0dc4f75d58cdd1b9
nikinov <novellenicholas@gmail.com>
2019-10-28 23:26:45 +0100

    WOW this is cool

---
+++ BALL RACE 0.2/Assets/scripts/InUse/Animation/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private Animator panel;
+    [SerializeField] private Animator cam;
+
+    public bool NotFirstTim;
+
+    private void Update()
+    {
+        panel.SetBool("NotFirstTime", NotFirstTim);
+    }
+    private void Awake()
+    {
+        NotFirstTim = true;
+    }
+}
